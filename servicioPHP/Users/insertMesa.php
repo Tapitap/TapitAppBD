63c8dd4d1f467ca38de2fb04d37da85d351d3fc3
Fonx95 <alfonsomasacalderon@gmail.com>
2022-05-29 22:21:01 +0200

    arreglo

--- servicioPHP/Users/insertMesa.php
+++ servicioPHP/Users/insertMesa.php
@@ -14,15 +14,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         
 		$retorno = Users::insertUser($username,$password);
 		
-        if($retorno!=-1){
+        if($retorno != '-1'){
 			
 			$retorno = Users::insertAuthority($username);
 			
-			if($retorno!=-1){
+			if($retorno!='-1'){
 				
 				$retorno = Users::insertMesa($username,$id_manager,$numero);
 				
-				if ($retorno!=-1) {
+				if ($retorno!='-1') {
             
 					$json["estado"] = "1";
 					$json["id"] = $retorno;
